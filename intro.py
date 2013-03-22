fd007a5b66255a2a8c44419c762a8fd180ec6741
Smotko <ensmotko@gmail.com>
2013-03-22 13:00:03 +0100

    Finished game

--- intro.py
+++ intro.py
@@ -12,26 +12,82 @@ clock = Clock()
 WIDTH  = 640
 HEIGHT = 480 
 
+score = 0
+
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pygame!")
 screen = pygame.display.get_surface()
 
-PLAYER_MOVE_FACTOR = .4
-ENEMY_MOVE_FACTOR  = .2
+PLAYER_MOVE_FACTOR = 1
+ENEMY_MOVE_FACTOR  = 1
+
 # SPRITES:
-
-hipster_path = os.path.join("assets", "hipster.png")
-hipster_surface = pygame.image.load(hipster_path)
-hipster_pos = [WIDTH/2-64,-128]
-
-superman_path  = os.path.join("assets", "superman.png")
-superman_surface = pygame.image.load(superman_path)
-superman_pos = [WIDTH/2,HEIGHT/2]
-superman_dir = [0,0]
 
 background = pygame.Surface(screen.get_size())
 background = background.convert()
 background.fill((250, 250, 250))
+
+font = pygame.font.Font(None, 36)
+class AnimatedSprite(pygame.sprite.Sprite):
+    def __init__(self, filename, num_anims, fps = 10):
+        pygame.sprite.Sprite.__init__(self)
+        
+        
+        self._images = self._create_images(filename, num_anims)
+        self._start = pygame.time.get_ticks()
+        self._delay = 1000 / fps
+        self._last_frame  = 0
+        self._frame = 0
+        self._acum = 0
+
+        self.rect = self._images[0].get_rect()
+        self.direction = [0,0]
+        # Call update to set our first image.
+        self.update(pygame.time.get_ticks())
+
+    def update(self, t):
+        self._acum += t
+        if self._acum > self._delay:
+            self._frame += 1
+            self._frame %= len(self._images)
+            self.image = self._images[self._frame]
+            self._acum = 0
+
+    def _create_images(self, filename, num_anims):
+        image = pygame.image.load(os.path.join('assets', filename)).convert_alpha()
+    
+        w, h = image.get_size()
+        return [image.subsurface((i*h, 0, h, h)) for i in xrange(num_anims)]
+
+class HipsterSprite(AnimatedSprite):
+    def update(self, delta):
+        AnimatedSprite.update(self, delta)
+        self.rect.centery += ENEMY_MOVE_FACTOR
+
+        if self.rect.top > HEIGHT: 
+            pass
+
+        if self.rect.colliderect(superman.rect.inflate(-128, -128)):
+            global score
+            score += 1
+            explosion.rect.center = self.rect.center
+            self.rect.left = random()*(WIDTH-self.rect.width)
+            self.rect.bottom = 0
+
+class SupermanSprite(AnimatedSprite):
+    
+    def update(self, delta):
+        AnimatedSprite.update(self, delta)
+        self.rect.centerx += self.direction[0] * delta * PLAYER_MOVE_FACTOR
+        self.rect.centery += self.direction[1] * delta * PLAYER_MOVE_FACTOR
+        
+            
+explosion = AnimatedSprite("explosion.png", 7)
+explosion.rect.center = (-100, -100)
+superman = SupermanSprite("superman.png", 1)
+superman.rect.midbottom = (WIDTH/2, HEIGHT)
+hipster = HipsterSprite("hipster.png", 3)
+all_sprites = pygame.sprite.RenderPlain((explosion,hipster,superman))
 
 # INPUT:
 
@@ -40,12 +96,12 @@ def input_key_down(key):
         sys.exit(0)
     for i,k in enumerate([K_LEFT, K_UP, K_RIGHT, K_DOWN]):
         if key == k:
-            superman_dir[i%2] += 1 * (0.1+i-2)/abs(0.1+i-2)
+            superman.direction[i%2] += 1 * (0.1+i-2)/abs(0.1+i-2)
 
 def input_key_up(key):
     for i,k in enumerate([K_RIGHT, K_DOWN, K_LEFT, K_UP]):
         if key == k:
-            superman_dir[i%2] += 1 * (0.1+i-2)/abs(0.1+i-2)
+            superman.direction[i%2] += 1 * (0.1+i-2)/abs(0.1+i-2)
        
 def input(events):
     for event in events:
@@ -56,43 +112,21 @@ def input(events):
         elif event.type == KEYDOWN:
             input_key_down(event.key)
 
-def are_colliding():
-    for i in [0,1]:
-        if superman_pos[i]+64+34 < hipster_pos[i]+64-34 or \
-           superman_pos[i]+64-34 > hipster_pos[i]+64+34:
-            return False
-    return True
-
 # UPDATE: 
 def update(delta):
     # move the hipster
-    superman_pos[0] += superman_dir[0] * delta * PLAYER_MOVE_FACTOR
-    superman_pos[1] += superman_dir[1] * delta * PLAYER_MOVE_FACTOR
-
-    for i in [0,1]:    
-        if superman_pos[i] < 0:
-            superman_pos[i] = 0
-
-    for i,s in enumerate([WIDTH, HEIGHT]):
-        if superman_pos[i] > s-128:
-            superman_pos[i] = s-128
-
-    hipster_pos[1] += delta * ENEMY_MOVE_FACTOR
-
-    
-    if hipster_pos[1] > HEIGHT + 128 or are_colliding():
-        hipster_pos[0] = random()*(WIDTH-128)
-        hipster_pos[1] = -128
-
-    
-
-# DRAW: 
+    all_sprites.update(delta)
+    # DRAW: 
 
 
 def draw():
     screen.blit(background, (0,0))
-    screen.blit(hipster_surface, hipster_pos)
-    screen.blit(superman_surface, superman_pos)
+    all_sprites.draw(screen)
+    font = pygame.font.Font(None, 42)
+    text = font.render(str(score), 1, (10, 10, 10))
+    textpos = text.get_rect()
+    textpos.centerx = background.get_rect().centerx
+    screen.blit(text, textpos)
     pygame.display.flip()
 
 while True:
